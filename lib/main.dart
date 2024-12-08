6ea8e1c079afbc6f62afe7cf69f7449cf8d58bcb
nelsonelielton <nelsonelielton@gmail.com>
2024-12-08 17:31:26 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'layout',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 79, 0, 121)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Nelson Elielton Bolkota '),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                // color: Colors.amber,
+                child: const Text('Inácio Martins'),
+              ),
+            ),
+            Expanded(
+              flex: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                        // alignment: Alignment.center,
+                        // color: Colors.red,
+                        // child: const Text('Primeira Coluna'),
+                        ),
+                  ),
+                  Expanded(
+                    flex: 15,
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 139, 95, 173),
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black45,
+                            blurRadius: 15,
+                            offset: Offset(8, 8),
+                          ),
+                        ],
+                      ),
+                      child: const Text('telento-tech'),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                        // alignment: Alignment.center,
+                        // color: Colors.red,
+                        // child: const Text('Terceira Coluna'),
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.center,
+                //color: Colors.blue,
+                child: const Text('ariangelo me da um estágio🙏'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.thumb_up),
+      ),
+    );
+  }
+}
